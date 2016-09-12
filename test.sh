4fb2b96f7c430e723f271a638d2df1ddfab8ad6a
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-09-12 13:30:58 -0700

    for ubuntu check

--- test.sh
+++ test.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
+echo "Tests start"
+
+function test(){
+  expected="$1"
+  result="$2"
+  echo "$result" | ./ccc > tmp.s
+
+  #コンパイルチェック
+  if [ ! $? ];then
+    echo "Failed to comile $result"
+    exit
+  fi
+
+  gcc -o tmp.out driver.c tmp.s || exit
+}
+
 
 make -s ccc
+
+test 0 0
+
+rm -f tmp.out tmp.s
+echo "All tests passed"
