9364bbc4734efd4b9aeb902ed05127702549d142
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-09-13 13:24:50 -0700

    input integer support

--- test.sh
+++ test.sh
@@ -12,12 +12,22 @@ function test(){
     exit
   fi
 
+  #tmp.outという実行ファイルを作る driver.cとtmp.cを使って
   gcc -o tmp.out driver.c tmp.s || exit
+  rlt="`./tmp.out`" #実行ファイルを実行
+
+  if [ "$rlt" != "$expected" ]; then
+    echo "Test failed $expected extected but got $rlt"
+    exit
+  fi
+
 }
 
 make -s ccc
 
+#test
 test 0 0
+test 42 42 
 
-rm -f tmp.out tmp.s
+# rm -f tmp.out tmp.s
 echo "All tests passed"
