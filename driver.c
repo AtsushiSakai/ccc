8daa067b7e141cf09ddbdc88b803b79aa0bcc598
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-09-15 13:55:05 -0700

    first release

--- driver.c
+++ driver.c
@@ -4,6 +4,7 @@ extern int mymain(void);
 
 int main(int argc, char **argv){
   int val=mymain();
-  printf("%d\n",val);
+  /* printf("aaa\n"); */
+  printf("%d\n",val);//この表示された値が、返り値
   return 0;
 }
--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ function test(){
 
   #tmp.outという実行ファイルを作る driver.cとtmp.cを使って
   gcc -o tmp.out driver.c tmp.s || exit
-  rlt="`./tmp.out`" #実行ファイルを実行
+  rlt="`./tmp.out`" #実行ファイルを実行して、標準出力を取得
 
   if [ "$rlt" != "$expected" ]; then
     echo "Test failed $expected extected but got $rlt"
@@ -29,5 +29,5 @@ make -s ccc
 test 0 0
 test 42 42 
 
-# rm -f tmp.out tmp.s
+rm -f tmp.out tmp.s
 echo "All tests passed"
